25823af667ed93150ff550ae43ed5dbde89a4d29
NightBurgerus <terehov.pasha@yandex.ru>
2022-04-24 10:12:47 +0300

    Initialization

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Application
+//
+//  Created by Паша Терехов on 24.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
